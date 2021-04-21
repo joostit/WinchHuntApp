ecd533a4bf4f519334f50992dcbf7c3e63e4383c
Joost Haverkort <dev@joostit.com>
2021-04-21 13:58:10 +0200

    Got the new Maps component to work with ApiKey injection

--- WinchHuntApp/WinchHuntApp/Client/Services/MapService.cs
+++ WinchHuntApp/WinchHuntApp/Client/Services/MapService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.JSInterop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -11,26 +12,30 @@ namespace WinchHuntApp.Client.Services
     public class MapService : IMapService
     {
 
-        public static int counter = 0;
-
         public MapsSettings Settings { get; private set; } = new MapsSettings();
         private PublicHttpClient http;
+        private readonly IJSRuntime jsRuntime;
 
         public string ApiKey { get; set; }
 
-        public MapService(PublicHttpClient http)
+        public MapService(PublicHttpClient http, IJSRuntime js)
         {
+            jsRuntime = js;
             this.http = http;
-
-            counter++;
-            Console.WriteLine($"MapService instances {counter}");
 
         }
 
         public async Task Initialize()
         {
             ApiKey = await GetMapsApiKey();
-        }    
+
+            await InitMapsJs();
+        }
+
+        private async Task InitMapsJs()
+        {
+            await jsRuntime.InvokeVoidAsync("initMapsScript", ApiKey);
+        }
 
         private async Task<string> GetMapsApiKey()
         {
---
+++ WinchHuntApp/WinchHuntApp/Client/wwwroot/js/mapsUtils.js
@@ -0,0 +1,27 @@
+﻿window.initMapsScript = function (apiKey) {
+    console.log("Window.InitMapsScript called");
+
+
+
+	let scriptsIncluded = false;
+	let scriptTags = document.querySelectorAll('head > script');
+	scriptTags.forEach(scriptTag => {
+		if (scriptTag.getAttribute('src').startsWith("https://maps.googleapis.com/maps/api/js?key=")) {
+			scriptsIncluded = true;
+			console.log("Google maps API script already included");
+			return;
+		}
+	});
+
+	console.log("Injecting Google Maps API script to <head>");
+
+	let src = "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&v=3";
+
+	console.log("Injecting src: " + src);
+
+	let importedMaps = document.createElement('script');
+	importedMaps.src = src;
+	importedMaps.defer = true;
+	document.head.appendChild(importedMaps);
+
+};
