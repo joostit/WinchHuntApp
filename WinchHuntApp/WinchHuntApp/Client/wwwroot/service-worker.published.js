b4e35825daf78023ea436a836ac7f14b2c963e2a
Joost Haverkort <dev@joostit.com>
2021-04-23 08:56:59 +0200

    Improved the Service worker to allow app updates more easlily Also cleaned up the AppSettings files to not contain secrets

--- WinchHuntApp/WinchHuntApp/Client/wwwroot/service-worker.published.js
+++ WinchHuntApp/WinchHuntApp/Client/wwwroot/service-worker.published.js
@@ -1,14 +1,20 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
+const BUILD_VERSION = 1.00000000;
+console.log("Running service worker build version: " + BUILD_VERSION);
+
 self.importScripts('./service-worker-assets.js');
-self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(onInstall(event));
+});
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/, /\.svg$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 async function onInstall(event) {
