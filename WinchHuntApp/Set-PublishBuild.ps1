4a010302e1f2b1000265b9c2e5f4a634308294d1
Joost Haverkort <dev@joostit.com>
2021-04-23 17:37:38 +0200

    Updated Set-Publish build to write the build number to the constants

--- WinchHuntApp/Set-PublishBuild.ps1
+++ WinchHuntApp/Set-PublishBuild.ps1
@@ -1,1 +1,13 @@
-((Get-Content -path $args[0] -Raw) -replace '0.1010101010',$args[1]) | Set-Content -Path $args[0]
+$serviceWorkerPath = 'WinchHuntApp\WinchHuntApp\Client\wwwroot\service-worker.published.js'
+
+$clientBuildPath = 'WinchHuntApp\WinchHuntApp\Client\Properties\ClientBuild.cs'
+$serverBuildPath = 'WinchHuntApp\WinchHuntApp\Server\Properties\ServerBuild.cs'
+
+$DevBuildPlaceholder = 'DEV000000000#'
+
+Write-Host 'Setting build number constants to: $args[0]'
+
+((Get-Content -path $serviceWorkerPath -Raw) -replace '$DevBuildPlaceholder',$args[0]) | Set-Content -Path $serviceWorkerPath
+
+((Get-Content -path $clientBuildPath -Raw) -replace '$DevBuildPlaceholder',$args[0]) | Set-Content -Path $clientBuildPath
+((Get-Content -path $serverBuildPath -Raw) -replace '$DevBuildPlaceholder',$args[0]) | Set-Content -Path $serverBuildPath
